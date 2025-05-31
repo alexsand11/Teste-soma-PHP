dcfab697f21b46da0b361cd23f914272a99ac30d
alexsand11 <alex5638h@gmail.com>
2025-05-31 12:14:04 -0300

    Add files via upload

---
+++ teste.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="pt-br">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Teste de Soma em PHP</title>
+   </head>
+
+  <body>
+  <?php
+    $a = 1;
+    $b = 2;
+    $soma = $b+ $a;
+    echo "A soma de $a + $b é igual a $soma";
+
+    if ($soma = 1) 
+    false
+   
+    
+
+  ?>
+
+</body>
+
+</html>
